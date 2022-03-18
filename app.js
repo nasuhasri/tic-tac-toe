701beb7e017160e6bfd38f935974552980153b5e
Nasuha Asri <nasuhasri00@gmail.com>
2022-03-18 17:03:12 +0800

    add winning condition

--- app.js
+++ app.js
@@ -13,22 +13,28 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.listen('3000');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.redirect('/tic');
+});
+
 /** Tambahan **/
 app.use('/tic', ticRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/tic.js
+++ routes/tic.js
@@ -1,95 +1,133 @@
-'use strict'
+'use strict';
 
 var express = require('express');
 const { NotImplemented } = require('http-errors');
 var router = express.Router();
 
 var gridArray = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+];
 
 /* First User buat 0. Second user buat X */
-var isFirstUSer = true
+var isFirstUSer = true;
 
-var firstMarker = '0'
-var altMarker = 'X'
+var firstMarker = '0';
+var altMarker = 'X';
 
-var isWin = false
+var isWin = false;
 
 /* GET users listing. */
 /* Ni untuk display */
-router.get('/', function(req, res, next) {
-    var winning = ''
-    if(isWin){
-        winning = (isFirstUSer ? firstMarker : altMarker) + ' have won'
-    }
-    res.render('index', { 
-        grid: gridArray, 
-        isWin: isWin,
-        winner: isWin ? (isFirstUSer ? firstMarker : altMarker): '',
-        winningText: winning });
+router.get('/', function (req, res, next) {
+  var winning = '';
+  if (isWin) {
+    winning = (isFirstUSer ? firstMarker : altMarker) + ' have won';
+  }
+  res.render('index', {
+    grid: gridArray,
+    isWin: isWin,
+    winner: isWin ? (isFirstUSer ? firstMarker : altMarker) : '',
+    winningText: winning,
+  });
 });
 
 /* Ni untuk update location then redirect ke page tic */
 /* Everytime dia masuk new location kena check sama ada dia dah menang/belum */
-router.get('/:x/:y', function(req, res, next) {
+router.get('/:x/:y', function (req, res, next) {
+  if (isWin) {
+    return;
+  }
 
-    if(isWin) {
-        return
+  if (isFirstUSer) {
+    // assign first marker: 0 to first user
+    gridArray[req.params.x][req.params.y] = firstMarker;
+  } else {
+    // assign second marker: X to second user
+    gridArray[req.params.x][req.params.y] = altMarker;
+  }
+
+  let marker = '';
+
+  if (isFirstUSer) {
+    marker = firstMarker; // 0
+  } else {
+    marker = altMarker; // X
+  }
+
+  /* Winning condition */
+  /* Check row winning */
+  for (var x = 0; x < gridArray.length; x++) {
+    // return element that same as marker
+    // filter for row
+    const row = gridArray[x].filter((element) => element === marker);
+
+    // if number of element is same as the gridArray length, then user wins
+    // eg row 0 has 3, the num same as gridArray length in any row so it wins
+    // row condition
+    if (row.length == gridArray[x].length) {
+      isWin = true;
+      console.log('row condition fired');
+      break;
     }
 
-    if(isFirstUSer){
-        gridArray[req.params.x][req.params.y] = firstMarker
-    }
-    else{        
-        gridArray[req.params.x][req.params.y] = altMarker
-    }
-
-    /* Winning condition */
-    /* Check row winning */
-    for(var x = 0; x < gridArray.length;  x++){
-        
-        let marker = ''
-        if(isFirstUSer){
-            marker = firstMarker
-        }
-        else {
-            marker = altMarker
-        }
-
-        const row = gridArray[x].filter(element => element === marker)
-
-        if(row.length == gridArray[x].length){
-            isWin = true
-            break
-        }
+    // column condition
+    if (
+      gridArray[0][x] == gridArray[1][x] &&
+      gridArray[1][x] == gridArray[2][x] &&
+      gridArray[2][x] != ''
+    ) {
+      isWin = true;
+      console.log('column condition fired');
+      break;
     }
 
-    if(!isWin) {
-        isFirstUSer = !isFirstUSer
+    // diagonal condition right-left
+    if (
+      gridArray[0][0] == gridArray[1][1] &&
+      gridArray[1][1] == gridArray[2][2] &&
+      gridArray[2][2] != ''
+    ) {
+      isWin = true;
+      console.log('diagonal condition right-left fired');
+      break;
     }
 
-    /* First value akan jadi true, so bila sampai kat line ni, dia akan simpan value false pula */
-    /* Reverse boolean so second user dapat jalan */
-    //isFirstUSer = !isFirstUSer;
-    res.redirect('/tic');
+    // diagonal condition left-right
+    if (
+      gridArray[2][0] == gridArray[1][1] &&
+      gridArray[1][1] == gridArray[0][2] &&
+      gridArray[0][2] != ''
+    ) {
+      isWin = true;
+      console.log('diagonal condition left-right fired');
+      break;
+    }
+  }
+
+  if (!isWin) {
+    isFirstUSer = !isFirstUSer;
+  }
+
+  /* First value akan jadi true, so bila sampai kat line ni, dia akan simpan value false pula */
+  /* Reverse boolean so second user dapat jalan */
+  //isFirstUSer = !isFirstUSer;
+  res.redirect('/tic');
 });
 
 /* Ni untuk reset the game bila tekan button new game */
-router.get('/new', function(req, res, next) {
+router.get('/new', function (req, res, next) {
+  gridArray = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
 
-    gridArray = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ]
-
-    /* Bila click new game, first user akan sentiasa jadi true */
-    isFirstUSer = true;
-    isWin = false;
-    res.redirect('/tic');
+  /* Bila click new game, first user akan sentiasa jadi true */
+  isFirstUSer = true;
+  isWin = false;
+  res.redirect('/tic');
 });
 
 module.exports = router;
